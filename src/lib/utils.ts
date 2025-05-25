92a7ff253ba6d28f64e5297f1c4e1c40b0d3bde8
minhhieple97 <hieplevuc@gmail.com>
2025-05-25 15:19:01 +0700

    feat: Enhance user role utilities with isUser and isAdmin functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { userRoleEnum } from '@/db/schema';
+import { User } from '@clerk/nextjs/server';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const isUser = (user: User) => {
+  return user.privateMetadata?.role?.toString() === userRoleEnum.enumValues[0];
+};
+
+export const isAdmin = (user: User) => {
+  return user.privateMetadata?.role?.toString() === userRoleEnum.enumValues[1];
+};
