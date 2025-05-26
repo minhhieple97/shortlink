9e77eecdbd4d2e7706ba4f8d7e6d1d5c68de2a9c
minhhieple97 <hieplevuc@gmail.com>
2025-05-26 08:45:26 +0700

    feat: Add middleware for authentication and role-based access control using Clerk

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { clerkMiddleware, createRouteMatcher, clerkClient } from '@clerk/nextjs/server';
+import { ROLE_TYPE } from '@/constants';
+const isAdminRoute = createRouteMatcher(['/admin', '/admin/(.*)']);
+
+const isAuthRoute = createRouteMatcher(['/dashboard', '/dashboard/(.*)']);
+
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard',
+  '/dashboard/(.*)',
+  '/checkout',
+  '/profile',
+  '/profile/(.*)',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
+  }
+  if (userId) {
+    const clerk = await clerkClient();
+    const user = await clerk.users.getUser(userId);
+    const role = user.privateMetadata?.role;
+    if (isAdminRoute(req) && role !== ROLE_TYPE.ADMIN) {
+      return Response.redirect(new URL('/', req.url));
+    }
+
+    if (isAuthRoute(req) && role !== ROLE_TYPE.USER) {
+      return Response.redirect(new URL('/', req.url));
+    }
+  }
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
